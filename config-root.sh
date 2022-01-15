39c20837604f4fc3633d0d545fe0f5451b1b9b74
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2022-01-14 19:01:38 -0800

    Adds terminator

--- config-root.sh
+++ config-root.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Base Packages
-apt install sudo build-essential git nodejs npm lightdm awesome awesome-extra network-manager w3m vim neovim neofetch compton rxvt-unicode zsh zplug python python3 python3-pip xsel chromium xsettingsd exa libxext-dev libxres-dev
+apt install sudo build-essential git nodejs npm lightdm awesome awesome-extra network-manager w3m vim neovim neofetch compton terminator rxvt-unicode zsh zplug python python3 python3-pip xsel chromium xsettingsd exa libxext-dev libxres-dev
 
 # Set up LightDM
 dpkg-reconfigure lightdm
